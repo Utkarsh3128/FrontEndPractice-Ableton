4051255ed2cb14b7b03f9241706558de04170b06
Utkarsh3128 <ukumar21-cse@bvucoep.edu.in>
2023-02-09 00:35:11 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      sans1: [
+        [
+          "futura-pt",
+          "Avenir",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "arial",
+          "sans-serif",
+        ],
+      ],
+    },
+    textColor: {
+      orangeColor: ["rgb(255,118,77)"],
+    },
+    backgroundPosition: {
+      "x-axis": ["50%"],
+    },
+    colors: {
+      facebook: '#3477f2',
+    }
+  },
+  plugins: [],
+};
+
+// futura-pt,Avenir,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Helvetica,Arial,sans-serif
+
